62219155953802c5023412d0ded03e5b7d5c5a5b
cocoa <>
2019-08-08 13:30:47 +0900

    commit 8 정렬 옵션 기능 구현
    - 정렬 옵션(오름차순, 내림차순) 선택 시 페이지 초기화 및 API 리스트 요청
    - 오름차순의 경우 API 응답 데이터(배열) 추가 정렬 로직 구현

--- src/components/ContentsList.vue
+++ src/components/ContentsList.vue
@@ -10,8 +10,8 @@
     />
     <div class="nav-tool">
       <button class="btn btn-info" @click="toggleModal">필터</button>
-      <button class="btn btn-primary">오름차순</button>
-      <button class="btn btn-danger">내림차순</button>
+      <button class="btn btn-primary" @click="switchSortOptionToAsc">오름차순</button>
+      <button class="btn btn-danger" @click="switchSortOptionToDesc">내림차순</button>
     </div>
     <div class="items-section">
       <ItemsContainer v-bind:itemList="itemList" />
@@ -36,6 +36,7 @@ export default {
       asc: 100,
       desc: 2
     },
+    switchedSortOption: "desc",
     isCategoryChecked: {
       category1: true,
       category2: true,
@@ -47,6 +48,7 @@ export default {
     toggleModal () {
       this.isModalOn = !this.isModalOn
     },
+
     // 카테고리 체크 상태 업데이트
     updateCheck (checked, boxName) {
       this.isCategoryChecked = {
@@ -55,32 +57,77 @@ export default {
       }
       console.log(this.isCategoryChecked)
     },
+
     getSearch () {
       console.log("API 요청")
     },
-    scrollRequestApi () {
+
+    // 오름차순 정렬
+    sortToAsc (arr) {
+      return arr.sort((a, b) => Number(a.no) - Number(b.no))
+    },
+
+    // 정렬 옵션 오름차순 전환
+    switchSortOptionToAsc () {
+      this.switchedSortOption = "asc"
+      this.requestListAfterSwitchSort((this.ord.asc = 100), this.sortToAsc)
+      this.ord.asc--
+      // 응답 데이터 배열 내부에서 다시 오름차순으로 정렬해야 한다.
+    },
+
+    // 정렬 옵션 내림차순 전환
+    switchSortOptionToDesc () {
+      this.switchedSortOption = "desc"
+      this.requestListAfterSwitchSort((this.ord.desc = 1))
+      this.ord.desc++
+    },
+
+    // 정렬 옵션 전환 후 초기 리스트 요청
+    requestListAfterSwitchSort (pageNum, sortLogic) {
+      this.itemList = []
+      this.$http
+        .get(`http://comento.cafe24.com/request.php/?page=${pageNum}`)
+        .then(response => {
+          if (sortLogic) {
+            const sortedList = sortLogic(response.data.list)
+            this.itemList = [...sortedList]
+          } else this.itemList = [...response.data.list]
+        })
+    },
+
+    // 스크롤링 시 리스트 요청
+    requestListWhenScroll () {
       const scrollHeight = document.documentElement.scrollHeight
       const pageYOffset = window.pageYOffset
       const innerHeight = window.innerHeight
+
       if (scrollHeight - pageYOffset - innerHeight === 0) {
         this.$http
-          .get(`http://comento.cafe24.com/request.php/?page=${this.ord.desc}`)
+          .get(
+            `http://comento.cafe24.com/request.php/?page=${
+              this.switchedSortOption === "asc" ? this.ord.asc : this.ord.desc
+            }`
+          )
           .then(response => {
-            this.itemList = [...this.itemList, ...response.data.list]
-            this.ord.desc++
+            if (this.switchedSortOption === "asc") {
+              const sortedList = this.sortToAsc(response.data.list)
+              this.itemList = [...this.itemList, ...sortedList]
+            } else this.itemList = [...this.itemList, ...response.data.list]
+            if (this.switchedSortOption === "asc") this.ord.asc--
+            else this.ord.desc++
           })
       }
     }
   },
-  // 컴포넌트 마운트 시 글 리스트 10개 GET 요청
+  // 컴포넌트 마운트 시 리스트 요청
   mounted () {
     console.log("Component mounted.")
     this.$http
-      .get("http://comento.cafe24.com/request.php/?page=1")
+      .get(`http://comento.cafe24.com/request.php/?page=${this.ord.desc - 1}`)
       .then(response => {
         this.itemList = [...response.data.list]
       })
-    window.addEventListener("scroll", this.scrollRequestApi)
+    window.addEventListener("scroll", this.requestListWhenScroll)
   }
 }
 </script>
